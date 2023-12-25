90ac2e413fc9a83491844707eb5663f71b932242
Ivaylo Dankolov <IvoDankolov@gmail.com>
2023-12-25 18:57:42 +0200

    Added day 2 part 2. Pretty trivial stfuff at least at input sizes where overflow doesn't matter.

--- src/solvers.rs
+++ src/solvers.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, path::PathBuf, fs::File, io::BufReader};
 mod day1p1;
 mod day1p2;
 mod day2p1;
+mod day2p2;
 
 struct Solver {
     solve: fn(input: BufReader<File>) -> ()
@@ -12,7 +13,8 @@ pub fn solve_day(day: String, input: Option<PathBuf>) {
     let solvers: HashMap<String, Solver> = HashMap::from([
         (String::from("1p1"), day1p1::SOLVER),
         (String::from("1p2"), day1p2::SOLVER),
-        (String::from("2p1"), day2p1::SOLVER)
+        (String::from("2p1"), day2p1::SOLVER),
+        (String::from("2p2"), day2p2::SOLVER)
     ]);
 
     let current_solver = solvers.get(&day).unwrap_or_else(|| panic!("Not a valid day to solve: {day}"));
---
+++ src/solvers/day2p2.rs
@@ -0,0 +1,74 @@
+use std::io::BufRead;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+use super::Solver;
+
+const NUM_REDS:i32   = 12;
+const NUM_GREENS:i32 = 13;
+const NUM_BLUES:i32  = 14;
+
+lazy_static! {
+    static ref LINE_REGEX: Regex = Regex::new(r"Game (\d+):(.*)").unwrap();
+    static ref ROUND_PART_REGEX: Regex = Regex::new(r" (\d+) (red|green|blue)").unwrap();
+}
+
+struct Round {
+    reds: i32,
+    blues: i32,
+    greens: i32
+}
+struct Game {
+    id: i32,
+    rounds: Vec<Round>
+}
+
+fn parse_round(round_str: &str) -> Round {
+    let mut round = Round {reds: 0, blues: 0, greens: 0};
+
+    round_str.split(',').for_each(|part| {
+        let part_info = ROUND_PART_REGEX.captures(part).unwrap_or_else(|| panic!("Round part must be valid: '{}'", part));
+        let count = part_info.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let kind = part_info.get(2).unwrap().as_str();
+        match kind {
+            "red" => round.reds = count,
+            "green" => round.greens = count,
+            "blue" => round.blues = count,
+            _ => panic!("Non-rgb part of round found")
+        }
+    });
+
+    round
+}
+fn parse_game(str: &String) -> Game {
+    let line_info = LINE_REGEX.captures(str).expect("Should be valid game line");
+    // TODO: Like, this is all guaranteed by \d+, but all these unwrap calls are icky.
+    // See if there's a parser library that's simple and low overhead
+    let id = line_info.get(1).unwrap().as_str().parse::<i32>().unwrap();
+    
+    let round_strs = line_info.get(2).unwrap().as_str().split(';');
+    return Game {
+        id: id,
+        rounds: round_strs.map(parse_round).collect()
+    }
+}
+
+fn min_power(game: Game) -> i32 {
+    let min_red = game.rounds.iter().map(|r| r.reds).max().unwrap();
+    let min_green = game.rounds.iter().map(|r| r.greens).max().unwrap();
+    let min_blue = game.rounds.iter().map(|r| r.blues).max().unwrap();
+
+    min_red * min_green * min_blue
+}
+
+pub const SOLVER: Solver = Solver {
+    solve: |input| {
+        let games = input.lines().map(|line|
+            parse_game(&line.unwrap())
+        );
+
+        let power_sum:i32 = games.map(min_power).sum();
+        
+        println!("{}", power_sum);
+    }
+};
